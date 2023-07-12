c8406853f89cfc4e23a8fbe97a3fe033bfe49bd0
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-11 21:25:46 -0500

    fetch then catch

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'accept': 'application/json'
+        },
+        body: JSON.stringify({'name': name, 'email': email})
+    })
+    .then(resp => resp.json())
+    .then(data => document.body.innerHTML = data.id)
+    .catch(error => {
+        alert('message')
+        document.body.innerHTML = error.message
+    })
+}
+
